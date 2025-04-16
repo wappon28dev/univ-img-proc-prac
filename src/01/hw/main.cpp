f6781a1cd8f03321e8e58cafd727333cfd739eb2
wappon28dev <wappon_28_dev@ruru.be>
2025-04-16 15:13:07 +0900

    📝 01

---
+++ src/01/hw/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+#include <opencv2/opencv.hpp>
+
+#define FILE_NAME "./me@2024-04-11.png"
+#define WINDOW_NAME "output"
+
+int main(int argc, const char *argv[])
+{
+  auto src_img = cv::imread(FILE_NAME);
+
+  if (src_img.empty())
+  {
+    fprintf(stderr, "読み込み失敗\n");
+    return -1;
+  }
+
+  cv::imshow(WINDOW_NAME, src_img);
+  cv::waitKey();
+
+  return 0;
+}
